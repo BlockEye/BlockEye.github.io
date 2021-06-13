98e42d34344feaa62ca598c1fa9df8e9b9cca10c
Ritu Gala <39478730+ritugala@users.noreply.github.com>
2021-06-13 22:32:00 +0530

    changes

---
+++ precache-manifest.e27ff3e639f058cd3c9f35fa867d663b.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958ed1f6389a7e8ae488a1b520d5449d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "142cee172a889bebc9de",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4154676dcfb2ff198fe9",
+    "url": "/static/css/main.48dac5f1.chunk.css"
+  },
+  {
+    "revision": "142cee172a889bebc9de",
+    "url": "/static/js/2.17e83158.chunk.js"
+  },
+  {
+    "revision": "4154676dcfb2ff198fe9",
+    "url": "/static/js/main.6aedd3d4.chunk.js"
+  },
+  {
+    "revision": "84ee81eb95759214e9ab",
+    "url": "/static/js/runtime-main.c98186d5.js"
+  },
+  {
+    "revision": "ac1432a494446f561bb860821b48ccf6",
+    "url": "/static/media/AgriChain.ac1432a4.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "563336a614fb5c0ce4bfed9f44b86fd5",
+    "url": "/static/media/Blockeye-Cropped.563336a6.png"
+  },
+  {
+    "revision": "7fc9fbdc8eee6fe5d067e391fccb88c4",
+    "url": "/static/media/CT_img.7fc9fbdc.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8a3c03228c27141f4ae7cdc433033d25",
+    "url": "/static/media/ai-bct-2-min.8a3c0322.png"
+  },
+  {
+    "revision": "28b17b2acb99d5458ebd58ab1d551072",
+    "url": "/static/media/blockchain-min.28b17b2a.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "4baca3e00e7f266a21426ab8146c4c1c",
+    "url": "/static/media/humanoid-min.4baca3e0.jpg"
+  },
+  {
+    "revision": "ac685d3638f498ba65a3ef6b6dee237f",
+    "url": "/static/media/temp.ac685d36.jpg"
+  }
+]);
